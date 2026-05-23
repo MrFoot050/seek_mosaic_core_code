41d13553824ee69110f7613bdfe4b4ea582bd0fa
Ruben Huerta <rubenangel0422@gmail.com>
2026-05-22 19:56:09 -0700

    Update graph_creation.py

--- graph_creation.py
+++ graph_creation.py
@@ -4,33 +4,29 @@ Average Temp vs BB Temp, Average Temporal Noise of Center Pixel vs BB Temp, and 
 of Whole Image vs BB Temp. 
 '''
 
-import numpy as np
-import matplotlib.pyplot as plt
+# Data Points
+blackbody_temp = [20, 22.5, 25, 27.5, 30]
+avg_temp_all_images = [20.3407, 23.3113, 25.7162, 28.2587, 30.618]
+temporal_noise_center_pixel = [0.0051, 0.0046, 0.0053, 0.0046, 0.0057]
+temporal_noise_whole_image = [0.0053, 0.0053, 0.0056, 0.0049, 0.0063]
+average_standard_deviation = [0.5682, 0.5921, 0.5987, 0.5557, 0.456]
 
-# Data Points for cycle 1
-BB_Temp_Cycle_1 = [20, 22.5, 25, 27.5, 30]
-Avg_temp_all_images = [21.5582, 24.228, 26.6307, 29.1463, 31.4535]
-Temporal_Noise_Center_Pixel = [0.00159, 0.0009, 0.0001, 0.001, 0.0008]
-Temporal_Noise_Whole_Image = [0.00115, 0.0009, 0.00007, 0.001, 0.0012]
-
-average_standard_deviation = [0.1975 ,0.1682 ,0.131 ,0.2074 ,0.1552]
-
-#Create polynomial for cycle 1 avergae temp
-cycle_1_coefficients_avg_temp = np.polyfit(BB_Temp_Cycle_1, Avg_temp_all_images, 1)
-cycle_1_polynomial = np.poly1d(cycle_1_coefficients_avg_temp)
+#Create polynomial for avergae temp
+coefficients_avg_temp = np.polyfit(BB_Temp_Cycle_1, Avg_temp_all_images, 1)
+polynomial = np.poly1d(cycle_1_coefficients_avg_temp)
 print('1D polynomial for Avg Temp vs BB Temp:', cycle_1_polynomial)
 
-# Plot dataset and polynomial for cycle 1 average temp
+# Plot dataset and polynomial for average temp
 plt.figure()
 plt.errorbar(
-    BB_Temp_Cycle_1,
-    Avg_temp_all_images,
+    blackbody_temp,
+    avg_temp_all_images,
     yerr = average_standard_deviation,
     fmt = 'o',          # marker style
     capsize = 5,        # little caps on bars
     label = 'Data'
 )
-plt.plot(BB_Temp_Cycle_1, cycle_1_polynomial(BB_Temp_Cycle_1))
+plt.plot(blackbody_temp, polynomial(blackbody_temp))
 plt.title('Average Temperature of Full Series vs BB Temperature')
 plt.xlabel('Black Body Temperature (°C)')
 plt.ylabel('Average Temperature Recorded(°C)')
@@ -38,15 +34,15 @@ plt.show()
 
 
 #------------------------------------------------------------------------------------------------------------------------
-#Create polynomial for cycle 1 temporal noise of center pixel
-cycle_1_coefficients_temeporal_noise_center_pixel = np.polyfit(BB_Temp_Cycle_1, Temporal_Noise_Center_Pixel, 1)
-cycle_1_polynomial_temporal_noise_center_pixel = np.poly1d(cycle_1_coefficients_temeporal_noise_center_pixel)
-print('1D polynomial for Avg Temporal Noise of Center Pixel vs BB Temp:', cycle_1_polynomial_temporal_noise_center_pixel)
+#Create polynomial for temporal noise of center pixel
+coefficients_temeporal_noise_center_pixel = np.polyfit(blackbody_temp, temporal_noise_center_pixel, 1)
+polynomial_temporal_noise_center_pixel = np.poly1d(coefficients_temeporal_noise_center_pixel)
+print('1D polynomial for Avg Temporal Noise of Center Pixel vs BB Temp:', polynomial_temporal_noise_center_pixel)
 
-# Plot dataset and polynomial for cycle 1 temporal noise of center pixel
+# Plot dataset and polynomial for temporal noise of center pixel
 plt.figure()
-plt.scatter(BB_Temp_Cycle_1, Temporal_Noise_Center_Pixel)
-plt.plot(BB_Temp_Cycle_1, cycle_1_polynomial_temporal_noise_center_pixel(BB_Temp_Cycle_1))
+plt.scatter(blackbody_temp, temporal_noise_center_pixel)
+plt.plot(blackbody_temp, polynomial_temporal_noise_center_pixel(blackbody_temp))
 plt.title('Average Temporal Noise of Center Pixel vs BB Temperature')
 plt.xlabel('Black Body Temperature (°C)')
 plt.ylabel('Average Temporal Noise Recorded')
@@ -54,15 +50,15 @@ plt.show()
 
 
 #------------------------------------------------------------------------------------------------------------------------
-#Create polynomial for cycle 1 temporal noise of center pixel
-cycle_1_coefficients_temeporal_noise_whole_image = np.polyfit(BB_Temp_Cycle_1, Temporal_Noise_Whole_Image, 1)
-cycle_1_polynomial_temporal_noise_whole_image = np.poly1d(cycle_1_coefficients_temeporal_noise_whole_image)
-print('1D polynomial for Avg Temporal Noise of Whole Image vs BB Temp:', cycle_1_polynomial_temporal_noise_whole_image)
+#Create polynomial for temporal noise of center pixel
+coefficients_temporal_noise_whole_image = np.polyfit(blackbody_temp, temporal_noise_whole_image, 1)
+polynomial_temporal_noise_whole_image = np.poly1d(coefficients_temporal_noise_whole_image)
+print('1D polynomial for Avg Temporal Noise of Whole Image vs BB Temp:', polynomial_temporal_noise_whole_image)
 
-# Plot dataset and polynomial for cycle 1 temporal noise of center pixel
+# Plot dataset and polynomial for temporal noise of center pixel
 plt.figure()
-plt.scatter(BB_Temp_Cycle_1, Temporal_Noise_Center_Pixel)
-plt.plot(BB_Temp_Cycle_1, cycle_1_polynomial_temporal_noise_whole_image(BB_Temp_Cycle_1))
+plt.scatter(blackbody_temp, temporal_noise_whole_image)
+plt.plot(blackbody_temp, polynomial_temporal_noise_whole_image(blackbody_temp))
 plt.title('Average Temporal Noise of Full Images vs BB Temperature')
 plt.xlabel('Black Body Temperature (°C)')
 plt.ylabel('Average Temporal Noise Recorded')
